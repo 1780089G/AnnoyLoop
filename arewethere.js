610503868416ff43d470b0e751a9189d07ca0d49
Unknown <1780089G@student.tp.edu.sg>
2018-01-17 20:13:28 +0800

    2nd commit

--- arewethere.js
+++ arewethere.js
@@ -3,8 +3,9 @@ var answwer = prompt("are we there yet?");
 while(answer !== "yes") {
   var answer = prompt("are we there yet?");
 }
-if(answer === "yes") {
-  alert("Yay, we made it!!!")
-} else {
-  var answer = prompt("are we there yet?")
-}
+alert("YAY, WE MADE IT!!!");
+
+//if(answer === "yes") {
+//  alert("Yay, we made it!!!")
+//} else {
+//}
