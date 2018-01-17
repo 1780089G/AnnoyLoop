fa6f05a0e65b8454f961b08ce8304b42fca80bb5
Unknown <1780089G@student.tp.edu.sg>
2018-01-17 20:27:21 +0800

    3rdcomm

--- arewethere.js
+++ arewethere.js
@@ -1,11 +1,19 @@
-var answwer = prompt("are we there yet?");
+// var answwer = prompt("are we there yet?");
 
-while(answer !== "yes") {
-  var answer = prompt("are we there yet?");
-}
-alert("YAY, WE MADE IT!!!");
+// while(answer !== "yes" && answer !== "yeah") {
+//   var answer = prompt("are we there yet?");
+// }
+// alert("YAY, WE MADE IT!!!");
 
 //if(answer === "yes") {
 //  alert("Yay, we made it!!!")
 //} else {
 //}
+
+//verion 2 
+var answer = prompt("are we there yet?");
+
+while(answer.indexOf("yes") === -1) { 
+  var answer = prompt("are we there yet?");
+}
+alert("YAY, WE MADE IT!!!");
