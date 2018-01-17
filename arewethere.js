c24d2327976dd1971d790216e1406f7912b9765a
Unknown <1780089G@student.tp.edu.sg>
2018-01-17 20:05:53 +0800

    1stcomm

---
+++ arewethere.js
@@ -0,0 +1,10 @@
+var answwer = prompt("are we there yet?");
+
+while(answer !== "yes") {
+  var answer = prompt("are we there yet?");
+}
+if(answer === "yes") {
+  alert("Yay, we made it!!!")
+} else {
+  var answer = prompt("are we there yet?")
+}
